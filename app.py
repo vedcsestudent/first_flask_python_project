822f9447cf77f9185034649f9547910ac8f637fc
vedcsestudent <116956477+vedcsestudent@users.noreply.github.com>
2023-04-22 15:49:34 +0000

    basic structure publishing

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+app = Flask(
+  __name__)  #initialize the flas constructor take the name as an argument
 
 print(__name__)
+jobs = [{
+  "id": 1,
+  "title": "data scientist",
+  "salary": "Rs200000",
+  "location": "Sanfrancisco"
+}, {
+  "id": 1,
+  "title": "data scientist",
+  "salary": "Rs2000e00",
+  "location": "Sanfranciscorr"
+}, {
+  "id": 1,
+  "title": "data scientist",
+  "salary": "Rs20r0000",
+  "location": "Sanfranciscor"
+}, {
+  "id": 1,
+  "title": "data scientist",
+  "salary": "Rs20r0000",
+  "location": ""
+}]
+
+
 @app.route("/")
 def hello_world():
-  return "Hello world"
+  return render_template("home.html", company="Alpha con",
+                         Jobs=jobs)  #it will render the template file
+
+
+@app.route("/jobs")
+def show_job():
+  return jsonify(jobs)
 
 
 if __name__ == "__main__":
